476fb30c9c8aebd498143cb79f6ab52799b98ba3
Alessio Cimarelli <jenkin@dataninja.it>
2015-04-26 17:59:49 +0200

    Add fontawesome lib and fetch data from google sheet

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  Tabletop.init({
+    key: '1vbKj-KBH4ZAeJu0oSEJXyRhMm-0JdQeqB1sE_2Gg8Hw',
+    callback: function(data, tabletop) {
+      console.log(data);
+    }
+  });
+};
